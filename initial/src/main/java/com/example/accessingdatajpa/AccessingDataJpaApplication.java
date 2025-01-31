24adfed041cfb936917c5c171f97c84616b6a541
jungha.kim <jungha.kim@timf.co.kr>
2025-01-31 18:30:14 +0900

    JPA Customer 예시 공부

--- initial/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
+++ initial/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -1,13 +1,68 @@
 package com.example.accessingdatajpa;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@EnableJpaAuditing
 @SpringBootApplication
 public class AccessingDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataJpaApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider(){
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository){
+		return(args -> {
+			// save a few customers
+			repository.save(Customer.builder()
+					.firstName("jungha")
+					.lastName("kim")
+					.createdDate(LocalDateTime.now())
+					.createBy("SYSTEM")
+					.lastModifiedDate(LocalDateTime.now())
+					.lastModifiedBy("SYSTEM")
+				.build());
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			repository.findAll().forEach(customer -> {
+				log.info(customer.toString());
+			});
+			log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findById(1L);
+			log.info("Customer found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Bauer").forEach(bauer -> {
+				log.info(bauer.toString());
+			});
+			log.info("");
+
+
+		});
 	}
 
 }
---
+++ initial/src/main/java/com/example/accessingdatajpa/Customer.java
@@ -0,0 +1,52 @@
+package com.example.accessingdatajpa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class Customer{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+    @Override
+    public String toString() {
+        return String.format(
+            "Customer[id=%d, firstName='%s', lastName='%s']",
+            id, firstName, lastName);
+    }
+}
